430bf2867f65548ae6e5c86c84c5600567ae7ce1
SELETECH Andrea Cazzaniga <a.cazzaniga@seletech.com>
2016-06-03 22:27:58 +0200

    stable version

--- RPI_Photobooth/camerathread.cpp
+++ RPI_Photobooth/camerathread.cpp
@@ -2,25 +2,22 @@
 #include "mainwindow.h"
 #include <QDebug>
 #include <QDateTime>
-#include <QProcess>
 
 void CameraThread::run()
 {
-    qDebug() << "camera thread started";
-    QProcess process;
+    qDebug() << "camera thread started";...
 
-    process.start("raspivid", QStringList() << "-t 10000");
-/*    unsigned int length = MainWindow::getInstance()->getCamera().getImageBufferSize(); // Header + Image Data + Padding
+    unsigned int length = MainWindow::getInstance()->getCamera().getImageBufferSize(); // Header + Image Data + Padding
     unsigned char * image = new unsigned char[length];
-    for (int i = 0; i < 1; i++) {
-        qDebug() << "new image"<< QDateTime::currentMSecsSinceEpoch();
+    //for (int i = 0; i < 1; i++) {
+    //    qDebug() << "new image"<< QDateTime::currentMSecsSinceEpoch();
         MainWindow::getInstance()->getCamera().grab_retrieve ( image,length );//get camera image
-        qDebug() << "grabbed" << QDateTime::currentMSecsSinceEpoch();
-        emit acquiringImage(image) ;
-    }*/
+    //    qDebug() << "grabbed" << QDateTime::currentMSecsSinceEpoch();
+    //    emit acquiringImage(image) ;
+    //}
 
 
     qDebug() << "emitting image ready signal..";
-    //emit imageReady(image) ;
+    emit imageReady(image) ;
 }
 
