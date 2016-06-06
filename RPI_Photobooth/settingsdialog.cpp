1c9eea41aa0184643a9345faff3d5372ff3ab10f
SELETECH Andrea Cazzaniga <a.cazzaniga@seletech.com>
2016-06-06 10:48:50 +0200

    added ISO combobox in settings ui

--- RPI_Photobooth/settingsdialog.cpp
+++ RPI_Photobooth/settingsdialog.cpp
@@ -3,6 +3,7 @@
 #include "mainwindow.h"
 #include <QDialog>
 #include <QButtonGroup>
+#include <QDebug>
 
 SettingsDialog* SettingsDialog::instance = nullptr;
 
@@ -15,6 +16,16 @@ SettingsDialog::SettingsDialog(QWidget *parent)
     ui->brightnessSlider->setValue(50);
     ui->contrastSlider->setRange(0,100);
     ui->contrastSlider->setValue(50);
+    ui->ISOCombo->addItem("100",100);
+    ui->ISOCombo->addItem("200",200);
+    ui->ISOCombo->addItem("300",300);
+    ui->ISOCombo->addItem("400",400);
+    ui->ISOCombo->addItem("500",500);
+    ui->ISOCombo->addItem("600",600);
+    ui->ISOCombo->addItem("700",700);
+    ui->ISOCombo->addItem("800",800);
+    ui->ISOCombo->setCurrentIndex(3);
+
     connect(ui->buttonBox, SIGNAL (accepted()),this, SLOT (updateCameraSettings()));
 }
 
@@ -28,7 +39,14 @@ SettingsDialog* SettingsDialog::getInstance()
     return instance;
 }
 
-void SettingsDialog::updateCameraSettings() {
+void SettingsDialog::updateCameraSettings()
+{
+
+    qDebug() << "Brightness: "  << instance->ui->brightnessSlider->value();
+    qDebug() << "Contrast: "    << instance->ui->contrastSlider->value();
+    qDebug() << "ISO: "         << instance->ui->ISOCombo->currentText().toInt();
+
     MainWindow::getInstance()->getCamera().setBrightness(instance->ui->brightnessSlider->value());
     MainWindow::getInstance()->getCamera().setContrast(instance->ui->contrastSlider->value());
+    MainWindow::getInstance()->getCamera().setISO(instance->ui->ISOCombo->currentText().toInt());
 }
